40379d2f4bdf06ef6bbded985ac0f2e65fc5675d
pooja062000 <vikaskosli9890@gmail.com>
2023-06-18 13:43:22 +0530

    new changes6

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -16,6 +16,56 @@ function Skills(){
                     <div className='skills-text'>HTML</div>
                     <div className='numerb'>100%</div>
                     <div className="skills-border1"></div>
+               
+                </div>
+                <div className='skills-box2'>
+                    
+                <div className='skills-color'></div>
+                <div className='skills-text'>CSS</div>
+                    <div className='numerb'>99%</div>
+                    <div className="skills-border2"></div>
+                </div>
+                </div>
+            <div className="skill-colum2">
+            <div className='skills-box3'>
+                    <div className='skills-color'></div>
+                    <div className='skills-text'>Bootstrap</div>
+                         <div className='numerb'>90%</div>
+                        
+                        <div className="skills-border3"></div>
+                
+                    </div>
+                    <div className='skills-box4'>
+                
+                <div className='skills-color'></div>
+                    <div className='skills-text'>JavaScript</div>
+                        <div className='numerb'>95%</div>
+                        
+                        <div className="skills-border4"></div>
+                         </div>
+                         </div>
+                    <div className="skills-col3">
+                    <div className='skills-box5'>
+                   
+                   <div className='skills-color'></div>
+                       <div className='skills-text'>Jquery</div>
+                           <div className='numerb'>96%</div> 
+                           
+                           <div className="skills-border5"></div>
+                       </div>
+                       <div className='skills-box6'>
+                           
+                   <div className='skills-color'></div>
+                       <div className='skills-text'>React JS</div>
+                           <div className='numerb'>50%</div>
+                           
+                           <div className="skills-border6"></div>
+                       </div>
+                    </div>
+                {/*<div className='skills-box1'>
+                    <div className='skills-text'>HTML</div>
+                    <div className='numerb'>100%</div>
+                    <div className="skills-border1"></div>
 
                 </div>
                 <div className='skills-box2'>
@@ -25,11 +75,11 @@ function Skills(){
                     <div className='numerb'>99%</div>
                     <div className="skills-border2"></div>
                 </div>
-                <div className='skills-box3'>
+               <div className='skills-box3'>
                     
                 <div className='skills-color'></div>
                 <div className='skills-text'>Bootstrap</div>
-                    <div className='numerb'>90%</div>
+    <div className='numerb'>90%</div>
                     
                     <div className="skills-border3"></div>
             
@@ -61,10 +111,11 @@ function Skills(){
                     <div className='numerb'>50%</div>
                     
                     <div className="skills-border6"></div>
-                </div>
-            </div>
-        </div>
-        </div>
+                </div>*/}
+    </div>
+    </div>
+        
+    
         </>
     )
 }
